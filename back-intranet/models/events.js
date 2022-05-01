0f1009fa7039883ab173b35970b3e9e8ab622c9d
Hector <89035945+hectoresen@users.noreply.github.com>
2022-05-01 15:22:21 +0200

    FEAT: Working on the events component

--- back-intranet/models/events.js
+++ back-intranet/models/events.js
@@ -5,8 +5,7 @@ const Schema = mongoose.Schema;
 const eventsSchema = new Schema(
     {
         eventTitle: {type: String, required: true},
-        dateAndDateTime: {type: Date, required: true},
-        description: {type: String, required: true},
+        eventDate: {type: Date, required: true},
         user: [{type: mongoose.Types.ObjectId, ref: 'Users'}]
     },
     {timestamps: true}
--- front-intranet/src/components/Events/Events.jsx
+++ front-intranet/src/components/Events/Events.jsx
@@ -1,34 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 import './Events.scss';
 
 const Events = () => {
-  const date = new Date();
-  const mapDate = {
-    dd: date.getDate(),
-    mm: date.toLocaleString('default', {month: 'long'}),
-    yy: date.getFullYear()
+  const [eventData, setEventData] = useState({eventTitle: '', eventDate: ''});
+
+  const currentDate = new Date();
+  const tomorrowDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+  const thirdDay = new Date(new Date().getTime() + 48 * 60 * 60 * 1000);
+  const fourthDay = new Date(new Date().getTime() + 72 * 60 * 60 * 1000);
+  console.log(fourthDay);
+
+
+  const mapToDate = (date) =>(
+    {
+      dd: date.getDate(),
+      ddStr: date.toDateString().substring(0, 3),
+      mm: date.toLocaleString('default', {month: 'long'}),
+      yy: date.getFullYear()
+    }
+  )
+
+  const inputEvents = (ev) =>{
+    const {name, value} = ev.target;
+    setEventData({...eventData, [name]: value});
   }
+  const submitEvents = (ev) =>{
+    ev.preventDefault();
+    console.log(eventData);
+  };
 
   return (
     <div className='events'>
       <div className='events__month'>
-        <p>{mapDate.mm}</p>
+        <p>Mes de {mapToDate(currentDate).mm}, {mapToDate(currentDate).yy}</p>
       </div>
       <div className='events__calendar'>
-        <div className='events__calendar-card'>
-          Dia 1
+        <div className='events__calendar__cards'>
+          <div className='events__calendar__cards-card'>
+            <p>{mapToDate(currentDate).ddStr}</p>
+            <p>{mapToDate(currentDate).dd}</p>
+          </div>
+          <div className='events__calendar__cards-card'>
+            <p>{mapToDate(tomorrowDate).ddStr}</p>
+            <p>{mapToDate(tomorrowDate).dd}</p>
+          </div>
+          <div className='events__calendar__cards-card'>
+            <p>Dia 3</p>
+          </div>
+          <div className='events__calendar__cards-card'>
+            Día 4
+          </div>
+          <div className='events__calendar__cards-card'>
+            Día 5
+          </div>
         </div>
-        <div className='events__calendar-card'>
-          Dia 2
-        </div>
-        <div className='events__calendar-card'>
-          Día 3
-        </div>
-        <div className='events__calendar-card'>
-          Día 4
-        </div>
-        <div className='events__calendar-card'>
-          Día 5
+
+        <div className='events__add'>
+          <Container className='events__add-form' maxWidth="xs">
+            <Box component="form" onSubmit={submitEvents} noValidate sx={{mt:1}}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="eventTitle"
+                label="Título de evento"
+                name="eventTitle"
+                type="text"
+                onChange={inputEvents}
+                autoFocus>
+              </TextField>
+              <TextField
+                margin="normal"
+                required
+                id="eventDate"
+                name="eventDate"
+                type="datetime-local"
+                onChange={inputEvents}
+                autoFocus>
+              </TextField>
+              <div>
+              <Button
+                type="submit"
+                margin="10px"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Crear
+              </Button>
+              </div>
+
+            </Box>
+          </Container>
         </div>
       </div>
     </div>
